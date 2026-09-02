05760288bd792b177dc40f7abd37d9b2c5af5b2f
MohamedCoderX <mdibrahim.developer@gmail.com>
2026-09-02 15:14:08 +0530

    Fix frontend: restore working state and remove backend/env/node_modules

--- backend/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
-cloudinary.config({
-  cloud_name: "dxp4ffpvt",
-  api_key: "699993624611399",
-  api_secret: "XbSElqrqfqw956nHNXa88nXKt6c",
-});
-
-module.exports = cloudinary;
--- backend/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-class ErrorHanlder extends Error{
-    constructor(message,statusCode){
-super(message)
-this.statusCode = statusCode;
-Error.captureStackTrace(this,this.constructor)
-    }
-}
-
-module.exports = ErrorHanlder
